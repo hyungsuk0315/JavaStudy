1d454cf77f68c729e8896bc23038541366d8e671
Administrator <Administrator@192.168.0.109>
2017-10-19 16:03:07 +0900

    1.IO/Calculate Upload

---
+++ Java_Basic/src/IOStream/Java_7287.java
@@ -0,0 +1,12 @@
+package IOStream;
+
+
+public class Java_7287 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("76");
+		System.out.println("hyungsuk0315");
+	}
+
+}
